8f5799d5a966c2cbcbf639c425fe155ca73a54e0
jihanlugas <jihanlugas2@gmail.com>
2020-05-11 23:54:46 +0700

    Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -19,9 +20,27 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('requested_by')->default(0);
+            $table->enum('is_complete', [0, 1]);
+            $table->string('invitation_code', 255)->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Jihan Lugas',
+                'email' => 'jihanlugas2@gmail.com',
+                'password' => Hash::make('123456'),
+
+            ],
+            [
+                'name' => 'Madina Milatil Arta',
+                'email' => 'madinamilatilarta@gmail.com',
+                'password' => Hash::make('123456'),
+            ],
+
+        ]);
     }
 
     /**
